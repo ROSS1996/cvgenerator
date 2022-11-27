9351405de90df4604ed20a63abf697f4fd9e9ed6
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-11-27 11:49:19 -0300

    Remove example fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="controlButtons">
+        <button type="button" className="editButton">Edit Mode</button>
+        <button type="button" className="printButton">Print</button>
+    </div>
+    <div id="curriculum">
+        <div id="header">
+            <div>
+                <h1 id="personName">John Smith</h1>
+                <h2 id='personRole'>IT Project Manager</h2>
+            </div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" alt="Candidate Portrait" className="photo" id='personPhoto' />
+        </div>
+        <section id="career">
+            <p id="personResume">
+                    IT Professional with more than 10 years of experience specializing in IT department management for international logistics companies.
+                    I can implement effective IT strategies at local and global levels.
+                    My greatest strength is business awareness.
+                    It enables me to permanently streamline infrastructure and applications.
+            </p>
+            <section id="experiencesSection">
+                <h3 className="section">Experience</h3>
+            </section>
+            <section id="educationSection">
+                <h3 className="section">Education</h3>
+                <ul id="education">
+                </ul>
+            </section>
+            <section id="coursesSection">
+                <h3 className="section">Courses</h3>
+                <ul id="courses">
+                </ul>
+            </section>
+        </section>
+        <section id="info">
+            <section id="personalData">
+                <h3 className="section">Personal Data</h3>
+                <div>
+                    <h4>Address</h4>
+                    <p id='address'>Washington, DC</p>
+                </div>
+                <div>
+                    <h4>Phone</h4>
+                    <p>774-987-4009</p>
+                </div>
+                <div>
+                    <h4>Email</h4>
+                    <a href="mailto://jsmith@uptwork.com">
+                        <p>jsmith@uptwork.com</p>
+                    </a>
+                </div>
+            </section>
+            <section id="socialmedia">
+                <h3 className="section">Social Media</h3>
+                <div>
+                </div>
+            </section>
+            <section id="keySkills">
+                <h3 className="section">Key Skills</h3>
+                <ul>
+                </ul>
+            </section>
+            <section id="techSkills">
+                <h3 className="section">Technical Skills</h3>
+                <ul>
+                </ul>
+            </section>
+            <section id="languages">
+                <h3 className="section">Languages</h3>
+                <ul>
+                    <li>
+                        <h4>English</h4>
+                        <p>Native</p>
+                    </li>
+                </ul>
+            </section>
+        </section>
+    </div>
     </div>
   )
 }
